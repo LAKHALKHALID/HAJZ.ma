9d0bd42a2a1cd81ca2fe9de6ac9f544981c1a1e6
khalid <khalidgit 512@gmail.com>
2025-12-27 17:25:31 +0100

    Create the Structure of the project and add images fiels

---
+++ app/src/index.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import "bootstrap/dist/css/bootstrap.min.css"
+import "bootstrap/dist/js/bootstrap.bundle.min.js"
+import "bootstrap-icons/font/bootstrap-icons.css"
+import ReactDOM from 'react-dom/client';
+
+
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    
+    
+  </React.StrictMode>
+);
+
+
+
