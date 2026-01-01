6e1f69b55f7ba35b800cc3bc03b465781ea45ef6
khalid <khalidgit 512@gmail.com>
2026-01-01 21:50:08 +0100

    change desing component hotel

--- app/src/user/hotels/listHotels.jsx
+++ app/src/user/hotels/listHotels.jsx
@@ -1,26 +1,42 @@
-import { useEffect } from "react";
+import { useEffect, useRef, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { getHotels } from "../../slices/sliceHotel";
 import Hotel from "./hotel";
+import Landing from "./landing";
 
 function Hotels() {
   const dispatch = useDispatch()
   const data = useSelector(st=>st.hotel.Hotels)
+
+  const [dataFilter,setDataFilter]=useState([])
+  const searsh = useRef()
   
   useEffect(function(){
 
     dispatch(getHotels())
 
   },[dispatch])
-  console.log(data);
+  
+  const afficher =()=>{
+    let newData = data.filter(h=>h.Ville === searsh.current.value)
+    setDataFilter(newData)
+    searsh.current.value=''
+  }
 
   return ( 
+    <>
+    <Landing afficher={afficher} sh={searsh} />
+    
     <div className="container">
     <h1 className="text-center">Page Hotels</h1>
       <div className="row">
-        {data.map((h,i)=><Hotel key={i} data={h} />)}
+        {
+        dataFilter.length === 0 ?data.map((h,i)=><Hotel key={i} data={h} />):dataFilter.map((h,i)=><Hotel key={i} data={h} />)
+        }
       </div>
     </div>
+
+    </>
   );
 }
 
